d23c67044005432aaadfb95da692f4b66fe9ee46
Eli3215 <elizabeth.selva@uned.cr>
2023-03-04 16:16:58 -0600

    Primer commit

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-mensaje = "Primer programa para verificar que PyCharm está bien instalado"
-
-if __name__ == '__main__':
-    print( mensaje )
+# Importación de módulos
 from os import system
-from time import sleep
 
-
+# Esta función permite limpiar la consola
+# Parámetros de entrada: Ninguno
+# Retorna: No
 def limpiar_consola():
     system('cls')
 
+# Esta función permite mostrar el menú al administrador de la caja registradora
+# Parámetros de entrada: Ninguno
+# Retorna: La opción para continuar con las opciones en caja
+def mostrar_menu_administrador():
+    print('Hola administrador, seleccione la acción que desea realizar:')
+    print('1. Ingresar productos al inventario')
+    print('2. Realizar una venta a un cliente')
+    print('3. Salir')
+    print('****************************************')
 
-for i in range(5):
-    print(f"Borrando la consola con cada ejecución!! {i+1}")
-    sleep(1)
-    limpiar_consola()
+    opcion = int(input('Ingrese la opción a realizar -> '))
+    return opcion
+
+# Inicio del programa
+if __name__ == "__main__":
+
+    # Este ciclo infinito permitirá consultar permanentemente las opciones del menú
+    # en la caja registradora
+    while True:
+        limpiar_consola()
+        opcion = mostrar_menu_administrador()
+
+        # En este condicional se verifica si ya no se desean realizar más acciones para salir del sistema
+        if opcion == 3:
+            limpiar_consola()
+            print('Fin de la jornada en la tienda!!!!')
+            break
